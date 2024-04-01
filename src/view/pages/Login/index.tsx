7c26eb19c34f00043afe9d145129e4010b0e6a58
José Carlos de Camargo <jccamargo15@gmail.com>
2024-03-31 23:44:15 -0300

    feat: Input component

--- src/view/pages/Login/index.tsx
+++ src/view/pages/Login/index.tsx
@@ -1,5 +1,36 @@
+import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+
 export function Login() {
   return (
-    <h1>Login page</h1>
+    <div>
+      <header className="flex flex-col items-center gap-4">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Entre em sua conta
+        </h1>
+
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">Novo por aqui?</span>
+          <Link to="/register" className="text-teal-900 font-medium tracking-[-0.5px]">
+            Crie uma conta
+          </Link>
+        </p>
+      </header>
+
+      <form className="flex flex-col mt-16 gap-4">
+        <Input
+          type="email"
+          placeholder="E-mail"
+          name="email"
+        />
+        <Input
+          type="password"
+          placeholder="Senha"
+          name="password"
+        />
+
+        <button type="submit" className="mt-2">Entrar</button>
+      </form>
+    </div>
   );
 }
