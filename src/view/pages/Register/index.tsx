274d52a619569337b0e7a16cec6db5980a579098
José Carlos de Camargo <jccamargo15@gmail.com>
2024-04-04 14:48:43 -0300

    feat: form register validation

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
+import { useRegisterController } from "./useRegisterController";
 
 export function Register() {
+  const { handleSubmit, register, errors } = useRegisterController();
+
   return (
     <>
       <header className="flex flex-col items-center gap-4 text-center">
@@ -18,20 +21,23 @@ export function Register() {
         </p>
       </header>
 
-      <form className="flex flex-col mt-16 gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col mt-16 gap-4">
         <Input
           placeholder="Nome"
-          name="nome"
+          error={errors.name?.message}
+          {...register("name")}
         />
         <Input
           type="email"
           placeholder="E-mail"
-          name="email"
+          error={errors.email?.message}
+          {...register("email")}
         />
         <Input
           type="password"
           placeholder="Senha"
-          name="password"
+          error={errors.password?.message}
+          {...register("password")}
         />
 
         <Button type="submit" className="mt-2">Criar conta</Button>
