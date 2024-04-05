c35a123d1d2c04915bcaf6cc9628a5c170358c02
José Carlos de Camargo <jccamargo15@gmail.com>
2024-04-05 17:35:25 -0300

    feat: auth context

--- src/Router/AuthGuard.tsx
+++ src/Router/AuthGuard.tsx
@@ -1,11 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../app/hooks/useAuth";
 
 interface AuthGuardProps {
   isPrivate: boolean;
 }
 
 export function AuthGuard({ isPrivate }: AuthGuardProps) {
-  const signedIn = false;
+  const { signedIn } = useAuth();
 
   if (!signedIn && isPrivate) {
     return <Navigate to="/login" replace />;
@@ -16,5 +17,5 @@ export function AuthGuard({ isPrivate }: AuthGuardProps) {
   }
 
   return <Outlet />;
-  
+
 }
