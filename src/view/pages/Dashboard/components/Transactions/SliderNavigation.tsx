3679912ea899f12b508bee85b0b27f0b085e36d0
José Carlos de Camargo <jccamargo15@gmail.com>
2024-04-16 23:12:36 -0300

    feat: transactions slider navigation background gradient

--- src/view/pages/Dashboard/components/Transactions/SliderNavigation.tsx
+++ src/view/pages/Dashboard/components/Transactions/SliderNavigation.tsx
@@ -7,14 +7,14 @@ export function SliderNavigation() {
   return (
     <>
       <button
-        className="absolute left-0 top-1/2 -translate-y-1/2 w-12 h-12 flex items-center justify-center bg-gray-100 z-10"
+        className="absolute left-0 top-1/2 -translate-y-1/2 w-12 h-12 flex items-center justify-center bg-gradient-to-r from-gray-100 to-transparent z-10"
         onClick={() => swiper.slidePrev()}
       >
         <ChevronLeftIcon className="w-6 h-6 text-gray-800" />
       </button>
 
       <button
-        className="absolute right-0 top-1/2 -translate-y-1/2 w-12 h-12 flex items-center justify-center bg-gray-100 z-10"
+        className="absolute right-0 top-1/2 -translate-y-1/2 w-12 h-12 flex items-center justify-center bg-gradient-to-l from-gray-100 to-transparent z-10"
         onClick={() => swiper.slideNext()}
       >
         <ChevronRightIcon className="w-6 h-6 text-gray-800" />
