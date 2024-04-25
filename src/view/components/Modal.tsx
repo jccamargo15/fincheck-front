c8336fc12fc49561067bba5fc4967d2b1182f0a5
José Carlos de Camargo <jccamargo15@gmail.com>
2024-04-25 00:02:27 -0300

    feat: fixed modal width

--- src/view/components/Modal.tsx
+++ src/view/components/Modal.tsx
@@ -15,7 +15,7 @@ export function Modal() {
         <Dialog.Content
           className={cn(
             'fixed top-1/2 left-1/2 max-h-[85vh] translate-x-1/2 translate-y-1/2 p-6 space-y-10 bg-white rounded-2xl z-[51] shadow-[Opx_11px_20px_0px_rgba(0,0,0,0.10)]',
-            'data-[state=open]:animate-content-show'
+            'data-[state=open]:animate-content-show w-full max-w-[400px]'
           )}
         >
           <h1>Modal</h1>
